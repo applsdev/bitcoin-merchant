a34ab06ce5aaeda1daeb1e8e41d78240cb536208
David FRANCOIS <david.francois@webflows.fr>
2011-05-16 15:08:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcoinMerchant::Application.config.secret_token = '9946515a34d6b1abd3efd3e94ff7dcff39d9bc067f6c2230bb7bc895c1ac06aa6a37d38b275203e7c3f8799a55b5adc824e23e5f1a9c5c4043e51bc2a409b1e9'
