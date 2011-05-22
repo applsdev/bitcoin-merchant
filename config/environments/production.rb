0ee99a3cc68091018f4993c57a20c3005af2c145
David FRANCOIS <david.francois@webflows.fr>
2011-05-22 23:51:38 +0200

    Couple of fixes and debugging options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ BitcoinMerchant::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+ config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
@@ -46,4 +46,7 @@ BitcoinMerchant::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # TODO : Remove me when going live
+  config.consider_all_requests_local       = true
 end
---
+++ config/initializers/spree.rb
@@ -0,0 +1,2 @@
+# TODO : Remove me when SSL vhosts are setup
+Spree::Config.set(:allow_ssl_in_production => false)
